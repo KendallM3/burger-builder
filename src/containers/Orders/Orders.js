0c5a138b3ca52452d4fe376abb56910211477065
Kendall MacMillan <kendall.macmillan@newtoneurope.com>
2020-09-07 10:28:34 +0100

    section 12: 228

---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import Order from '../../components/Order/Order';
+
+
+
+class Orders extends Component {
+    render () {
+        return (
+            <div>
+                <Order />
+                <Order />
+            </div>
+        );
+    }
+}
+
+export default Order;
+
+
+
